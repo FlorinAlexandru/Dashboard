7eef8496e19441197b9340df0509304b871e061f
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-04-23 20:05:38 +0300

    Add sidenav component

---
+++ src/app/sidenav/sidenav.component.ts
@@ -0,0 +1,10 @@
+import {Component, ViewEncapsulation} from '@angular/core';
+
+
+@Component({
+  selector: 'sidenav',
+  templateUrl: 'sidenav.component.html',
+  styleUrls: ['sidenav.component.css'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class Sidenav{}
