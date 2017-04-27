5c4aeddff2ad56b2758fbf0e8135b83a422b147d
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-04-27 09:30:03 +0300

    Use observable to get data

--- src/app/user-form/user-form.service.ts
+++ src/app/user-form/user-form.service.ts
@@ -1,21 +1,28 @@
 import { Injectable } from "@angular/core";
+import { Response, Http } from "@angular/http";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/operator/map";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/catch";
+
 
 @Injectable()
 export class UserService{
-    getUsers(): any[]{
-        return [
-        {
-            "id": 1,
-            "name": "John Doe",
-            "email": 'jd@email.com',
-            "age": 24
-        },
-        {
-            "id": 2,
-            "name": "Vick Duck",
-            "email": "vd@email.com",
-            "age": 29
-        }
-        ]
+    private url =  'http://localhost:8989/api/users';
+
+    constructor(private _http: Http){
+        
+    }
+
+    getUsers(): Observable<any[]>{
+        return  this._http.get(this.url)
+            .map((response: Response) => response.json())   
+            .do(data => console.log('All ' + JSON.stringify(data)))
+            .catch(this.errorHandler);
+    }
+
+    private errorHandler(error: Response){
+        console.log(error);
+        return Observable.throw(error.json().error || 'Server error');
     }
 }
