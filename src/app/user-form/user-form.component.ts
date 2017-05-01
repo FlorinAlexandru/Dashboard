8d445035ce33dee65e69748dc9e3467a9e2f30d4
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-05-01 22:25:04 +0300

    Add fom control vavriable && submit handler

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -1,28 +1,39 @@
 import { Component, OnInit } from "@angular/core";
+import { Response, Http } from "@angular/http";
+import 'rxjs/add/operator/map'
+
 import { UserService } from '../user-form/user-form.service';
 
 @Component({
     selector: 'user-form',
     templateUrl: 'user-form.component.html',
-    providers: [UserService]
+    styleUrls: ['user-form.component.css']
 })
 
-export class UserFormComponent implements OnInit{
+export class UserFormComponent{
 
-    pageTitle:string = 'User table';        
-    errorMsg: string = '';
-    userList:any[];
+    pageTitle:string = 'User form';        
+    nameField:string = '';
+    emailField:string = '';
+    ageField:number = 0;
+    submitResponse:any;
 
-    constructor(private _userService: UserService) {
+    constructor(private _http: Http){
 
     }
 
-    ngOnInit(): void{
-        this._userService.getUsers()
-            .subscribe(
-                users => this.userList = users,
-                error => this.errorMsg = <any>error
-            );
+    onSubmit():void {
+
+        var data: object = {
+            "name": this.nameField,
+            "email": this.emailField,
+            "age": this.ageField
+        };
+
+        this._http.post('http://localhost:8989/api/submit', data)
+            .map(res => res.json())
+            .do((data => console.log('All ' + JSON.stringify(data))))
+            .subscribe(result => this.submitResponse = result);
     }
     
 }
