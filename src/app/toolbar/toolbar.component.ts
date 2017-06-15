1650a766564ae07278f33649a3b1afb45f4a10b6
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-06-15 23:25:04 +0300

    Set title in toptoolbar when changing route

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router,NavigationStart } from '@angular/router';
 
 
 @Component({
@@ -6,6 +7,24 @@ import { Component } from '@angular/core';
     templateUrl: 'toolbar.component.html',
     styleUrls: ['toolbar.component.css']
 })
-export class ToolbarComponent{
+export class ToolbarComponent implements OnInit {
+    menuTitle: any;
+
+    constructor(private router: Router){
+
+    }
+
+    ngOnInit(){
+        this.menuTitle = this.router.events.subscribe(params => {
+            if(params instanceof NavigationStart) {
+                let temp = params.url;
+                temp = temp.split('/')[1].toUpperCase();
+                temp = temp.charAt(0).toUpperCase() + temp.substr(1).toLowerCase();
+                this.menuTitle = temp;
+            }
+            
+        });
+
+    }
 
 }
