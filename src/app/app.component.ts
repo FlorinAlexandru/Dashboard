e027b2c47debd9fb15088c9ed171bb9a938e0b61
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-06-17 01:10:53 +0300

    Subscribe to collapsible service&& change css class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CollapseService } from './common/collapse-service';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  subscription:Subscription;
+  flag: boolean;
+
+    constructor(private collapsibleService: CollapseService){ 
+    }
+
+
+    ngOnInit(){
+        this.subscription = this.collapsibleService.collapsibleItem$
+            .subscribe(flag => {
+              this.flag = flag
+              console.log('app component: ' + this.flag)
+            })
+    }
+
+
+    ngOnDestroy() {
+        // prevent memory leak when component is destroyed
+        this.subscription.unsubscribe();
+    }
 }
