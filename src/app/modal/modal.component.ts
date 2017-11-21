079fc5a0e3c18d12e49fc06f6090d31bae244d1e
florin marinescu <marinescu.florin.alexandru@gmail.com>
2017-11-22 00:29:05 +0200

    Implement modal view and logic

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,12 +1,29 @@
-import { Component, Input } from '@angular/core';
-import * as jQuery from 'jquery';
-
+import { Component, Input, OnInit } from '@angular/core';
+import { ModalService } from './modal.service';
 
 @Component({
     selector: 'modal-dialog',
-    templateUrl: 'modal.component.html'
+    templateUrl: 'modal.component.html',
+    styleUrls: ['modal.component.css']
 })
 
-export class ModalDialog{
+export class ModalDialog implements OnInit {
+    @Input() modalId: string;
+    @Input() bodyText: string;
+    @Input() titleText: string;
+    isOpen: boolean = false;
 
+
+    constructor(private modalService: ModalService){        
+    }
+
+
+    ngOnInit(){
+        this.modalService.register(this);
+    }
+
+    close(): void{
+        console.log('close modal');
+        this.modalService.close(this.modalId);
+    }
 }
